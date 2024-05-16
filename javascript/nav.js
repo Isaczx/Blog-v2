a42d45c0c91ae2433d884ecc24bec0e72980f9fa
Isac Santos <isacjobz@gmail.com>
2024-05-16 11:04:44 -0300

    ajuste
    foi realizado ajuste na aparição do menu  mobile

--- javascript/nav.js
+++ javascript/nav.js
@@ -7,9 +7,15 @@ menu.addEventListener("click" ,() =>{
     if (menu.id === "fechado") {
         menu.id = "aberto";
         listaMobile.style.display = "flex";
-    }else if(menu.id === "aberto"){
+        setTimeout(() => {
+            listaMobile.style.opacity = "1";
+        }, 25);
+    }else{
         menu.id = "fechado";
-        listaMobile.style.display = "none";
+        listaMobile.style.opacity = "0";
+        setTimeout(() => {
+            listaMobile.style.display = "none";
+        }, 250);
     }
 })
 
@@ -20,7 +26,9 @@ window.addEventListener('scroll', function() {
 
     if (scrollPosition > 100) { // Altere 100 para a posição desejada em que o menu deve se tornar fixo
         menu.classList.add('menu-fixed');
+        listaMobile.classList.add('menu-fixedL');
     } else {
         menu.classList.remove('menu-fixed');
+        listaMobile.classList.remove('menu-fixedL');
     }
 });
