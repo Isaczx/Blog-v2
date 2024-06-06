16d3698aa89cf52af80c4ec0d127961862e22ee5
Isac Santos <isacjobz@gmail.com>
2024-06-06 18:34:32 -0300

    foi adicionado a data
    foi colocado data e uma transição suave ao carrousel

--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
 var splide = new Splide( '.splide', {
     type   : 'loop',
     perPage: 4,
@@ -16,21 +17,61 @@ var splide = new Splide( '.splide', {
   } );
   
   splide.mount();
-
-
+  datas();
+ 
 var carrousel = document.querySelector(".splide");
 var setas = document.querySelector(".splide__arrows");
     
 carrousel.addEventListener("mouseenter", () =>{
   setas.style.opacity  = "1";
-})
+});
 
 carrousel.addEventListener("mouseleave", () =>{
   setas.style.opacity  = "0";
     
 
-})
+});
 
     
 
+function datas() {
+  // Seleciona todos os itens do carrossel
+  var itens = document.querySelectorAll(".carrousel");
 
+  // Itera sobre cada item do carrossel
+  itens.forEach(function(item) {
+      // Seleciona o elemento .titulo dentro do item atual
+      var titulo = item.querySelector(".titulo");
+      // Seleciona o elemento #data dentro do item atual
+      var data = item.querySelector("#data");
+
+      // Adiciona evento de clique ao item atual
+      item.addEventListener("mouseenter", () => {
+          if (data) {
+              data.style.display = "block";
+              titulo.style.top = "65%";
+              console.log("Item clicked");
+          }
+
+          
+      });
+
+      item.addEventListener("mouseleave", () => {
+        if (data) {
+            data.style.display = "none";
+            titulo.style.top = "70%";
+            console.log("Item clicked");
+        }
+
+        
+    });
+  });
+}
+
+// Chama a função após o carregamento do DOM
+document.addEventListener("DOMContentLoaded", datas);
+
+
+
+
+})
